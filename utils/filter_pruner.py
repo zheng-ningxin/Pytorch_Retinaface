61ce6ffed95f240051a44c99177d726bd43e1cdb
Ningxin <Ningxin.Zheng@microsoft.com>
2020-04-21 01:42:45 +0000

    Fix a bug when prune ratio equals to 0.
    Signed-off-by: Ningxin <Ningxin.Zheng@microsoft.com>

--- utils/filter_pruner.py
+++ utils/filter_pruner.py
@@ -13,7 +13,13 @@ class filter_pruner:
         w_abs = self.layer.ori_weight.abs()
         w_sum = w_abs.view(filters, -1).sum(1)
         count = filters - int( filters * ratio )
-        threshold = torch.topk(w_sum.view(-1), count, largest=False)[0].max()
+        if count > 0:
+            threshold = torch.topk(w_sum.view(-1), count, largest=False)[0].max()
+        else:
+            # don't prune this layer
+            threshold = torch.min(w_sum) - 1.0
+            # print(threshold)
+            
         mask_weight = torch.gt(w_sum, threshold)[:, None, None, None].expand_as(layer.weight).type_as(layer.w_mask).detach()
         need_b_mask = True if hasattr(layer, 'bias') and layer.bias is not None else False
         mask_bias = torch.gt(w_sum, threshold).type_as(layer.b_mask).detach() if need_b_mask else None
@@ -24,8 +30,11 @@ class filter_pruner:
         filters = self.layer.ori_weight.shape[0]
         w = self.layer.ori_weight.view(filters, -1)
         w_l2 = torch.sqrt((w ** 2).sum(dim=1)) 
-        count = filters - int( filters * ratio ) 
-        threshold = torch.topk(w_l2.view(-1), count, largest=False)[0].max()
+        count = filters - int( filters * ratio )
+        if count > 0: 
+            threshold = torch.topk(w_l2.view(-1), count, largest=False)[0].max()
+        else:
+            threshold = torch.min(w_l2) - 1.0
         mask_weight = torch.gt(w_l2, threshold)[:, None, None, None].expand_as(layer.weight).type_as(layer.w_mask).detach()
         need_b_mask = True if hasattr(layer, 'bias') and layer.bias is not None else False
         mask_bias = torch.gt(w_l2, threshold).type_as(layer.b_mask).detach() if need_b_mask else None
